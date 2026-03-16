2420e7aaee8133a8d9053bc096f07713dda886ef
stuctech-eng <stuctech@gmail.com>
2026-03-16 13:14:06 +0100

    Create  vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'BassFlow Pro',
+        short_name: 'BassFlow',
+        description: 'Jouw persoonlijke bas bibliotheek',
+        theme_color: '#FF2D7A',
+        background_color: '#F5F4F0',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ]
+})
