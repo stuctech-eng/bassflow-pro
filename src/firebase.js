2ca57f106bf316b7fe40a1492a30a6aba1d7ee62
stuctech-eng <stuctech@gmail.com>
2026-03-16 13:50:03 +0100

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6C7CzOEJuqWvVPWk2-lPJGHaqDjPGyxk",
+  authDomain: "bassflow-pro.firebaseapp.com",
+  databaseURL: "https://bassflow-pro-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bassflow-pro",
+  storageBucket: "bassflow-pro.firebasestorage.app",
+  messagingSenderId: "735579933666",
+  appId: "1:735579933666:web:e1c4480d1050a8469607bb"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
