3257aa63edc09b1c3482ce6043c4dec202f875b3
stuctech-eng <266315036+stuctech-eng@users.noreply.github.com>
2026-03-30 09:48:12 +0200

    r

--- src/firebase.js
+++ src/firebase.js
@@ -8,9 +8,7 @@ authDomain: "bassflow-pro.firebaseapp.com",
 projectId: "bassflow-pro",
 storageBucket: "bassflow-pro.firebasestorage.app",
 messagingSenderId: "735579933666",
-appId: "1:735579933666:web:e1c4480d1050a8469607bb"
-};
-
+appId: "1:735579933666:web:e1c4480d1050a8469607b
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
