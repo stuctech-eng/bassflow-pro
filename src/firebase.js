bad8b8bb92e5f39c0888647f4e709fb6f154a782
stuctech-eng <266315036+stuctech-eng@users.noreply.github.com>
2026-03-30 13:53:28 +0200

    oude

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-apiKey: "AIzaSyA6C7CzOEJuqWvVPWk2-lPJGHaqDjPGyxk",
-authDomain: "bassflow-pro.firebaseapp.com",
-projectId: "bassflow-pro",
-storageBucket: "bassflow-pro.firebasestorage.app",
-messagingSenderId: "735579933666",
-appId: "1:735579933666:web:e1c4480d1050a8469607bb"
+  apiKey: "AIzaSyA6C7CzOEJuqWvVPWk2-lPJGHaqDjPGyxk",
+  authDomain: "bassflow-pro.firebaseapp.com",
+  databaseURL: "https://bassflow-pro-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bassflow-pro",
+  storageBucket: "bassflow-pro.firebasestorage.app",
+  messagingSenderId: "735579933666",
+  appId: "1:735579933666:web:e1c4480d1050a8469607bb"
 };
 
 const app = initializeApp(firebaseConfig);
