d9fedf40860f1e08177007dabdf80ce0ee6c4c88
stuctech-eng <stuctech@gmail.com>
2026-03-30 09:53:47 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from “firebase/app”;
+import { getFirestore } from “firebase/firestore”;
+import { getStorage } from “firebase/storage”;
 
 const firebaseConfig = {
-apiKey: "AIzaSyA6C7CzOEJuqWvVPWk2-lPJGHaqDjPGyxk",
-authDomain: "bassflow-pro.firebaseapp.com",
-projectId: "bassflow-pro",
-storageBucket: "bassflow-pro.firebasestorage.app",
-messagingSenderId: "735579933666",
-appId: "1:735579933666:web:e1c4480d1050a8469607bb"
+apiKey: “AIzaSyA6C7CzOEJuqWvVPWk2-lPJGHaqDjPGyxk”,
+authDomain: “bassflow-pro.firebaseapp.com”,
+projectId: “bassflow-pro”,
+storageBucket: “bassflow-pro.firebasestorage.app”,
+messagingSenderId: “735579933666”,
+appId: “1:735579933666:web:e1c4480d1050a8469607bb”
 };
 
 const app = initializeApp(firebaseConfig);
